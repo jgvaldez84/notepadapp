70cc0d40b5ec90ceef6d0cb3cb32f0765b345c57
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-14 13:02:04 -0600

    created the initital state and created first reducer hook
    "set_notes"

--- src/App.js
+++ src/App.js
@@ -6,9 +6,26 @@ import 'antd/dist/antd.css'
 import { listnotes } from './graphql/queries'
 import './App.css';
 
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: {name: '', description: ''}
+
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+  case 'SET_NOTES':
+  return { ...state, notes: action.notes, loading: false }
+  case 'ERROR':
+  return { ...state, loading: false, error: true }
+  default: return state
+  }}
 
 function App() {
   
+  return 
 }
 
 export default App;
