98c7e2bc9dad8d4343298da1dd1337117c6315bc
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-14 13:54:19 -0600

    added code to view notes on the browser

--- src/App.js
+++ src/App.js
@@ -1,31 +1,75 @@
-import React, 
- { useEffect, useReducer } from 'react';
-import { API } from 'aws-amplify';
-import { List } from 'antd';
-import 'antd/dist/antd.css'
-import { listnotes } from './graphql/queries'
-import './App.css';
+import React, { useEffect, useReducer } from "react";
+import { API } from "aws-amplify";
+import { List } from "antd";
+import "antd/dist/antd.css";
+import { listNotes } from "./graphql/queries";
+import "./App.css";
 
 const initialState = {
   notes: [],
   loading: true,
   error: false,
-  form: {name: '', description: ''}
-
-}
+  form: { name: "", description: "" },
+};
 
 const reducer = (state, action) => {
-  switch(action.type) {
-  case 'SET_NOTES':
-  return { ...state, notes: action.notes, loading: false }
-  case 'ERROR':
-  return { ...state, loading: false, error: true }
-  default: return state
-  }}
+  switch (action.type) {
+    case "SET_NOTES":
+      return { ...state, notes: action.notes, loading: false };
+    case "ERROR":
+      return { ...state, loading: false, error: true };
+    default:
+      return { ...state };
+  }
+};
 
-function App() {
-  
-  return 
-}
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchNotes = async () => {
+    try {
+      const notesData = await API.graphql({
+        query: listNotes,
+      });
+      dispatch({ type: "SET_NOTES", notes: notesData.data.listNotes.items });
+      console.log(notesData)
+    } catch (err) {
+      console.error(err);
+      dispatch({ type: "ERROR" });
+    }
+  };
+
+  useEffect(()=> {
+    fetchNotes();
+  }, []);
+
+  const styles = {
+    container: {padding: 20},
+    input: {marginBottom: 10},
+    item: { textAlign: 'left' },
+    p: { color: '#1890ff' }
+    }
+
+  const renderItem = (item) => {
+    return (
+    <List.Item style={styles.item}>
+    <List.Item.Meta
+    title={item.name}
+    description={item.description}
+    />
+    </List.Item>
+    )
+    };
+
+  return (
+    <div style={styles.container}>
+    <List
+    loading={state.loading}
+    dataSource={state.notes}
+    renderItem={renderItem}
+    />
+    </div>
+    )
+};
 
 export default App;
